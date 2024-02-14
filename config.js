699103b6469a4d5bf1cc696b0d38d38526923795
alanacg <81997080+alanacg@users.noreply.github.com>
2024-02-14 13:26:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -18,18 +18,24 @@ var config = {
         {
             id: 'slug-style-id',
             alignment: 'left', //options are left, right, center, full
-            hidden: false,
+            hidden: false, //can hide panel with this
             title: 'Display Title',
             image: 'images/source.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             //text for description is html- can rely on that for extra formatting
             location: {
                 center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                zoom: 8.5,    //how close/far you are from map content
+                pitch: 60,    //angle from azimuth to view the map at (0 is straight on)
+                bearing: 0    //offset from 0°N map is viewed from
             },
-            mapAnimation: 'flyTo',
+            mapAnimation: 'flyTo',    //more options in mapbox/storytelling repo
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
             rotateAnimation: false,
             callback: '', //add additional js function for this key
             onChapterEnter: [
@@ -47,23 +53,17 @@ var config = {
             ]
         },
         {
-            id: 'second-identifier',
+            id: 'second-identifier', //need unique id for each chapter
             alignment: 'right',
             hidden: false,
             title: 'Second Title',
-            image: 'images/source.png',
+            image: 'images/target.svg',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [-77.020636, 38.886900],
                 zoom: 8.5,
                 pitch: 60,
                 bearing: -43.2,
-                // flyTo additional controls-
-                // These options control the flight curve, making it move
-                // slowly and zoom out almost completely before starting
-                // to pan.
-                //speed: 2, // make the flying slow
-                //curve: 1, // change the speed at which it zooms out
             },
             mapAnimation: 'flyTo',
             rotateAnimation: true,
@@ -109,5 +109,8 @@ var config = {
             onChapterEnter: [],
             onChapterExit: []
         }
+    //Add any new chapters above this line
+        
+    //Do not edit the two brackets below this line
     ]
 };
